53e866e188811637e1575d2f3782345425168591
heptaluan <vd2o@foxmail.com>
2022-08-20 20:03:58 +0800

    add

--- 007/js/box3_left.js
+++ 007/js/box3_left.js
@@ -1,52 +1,55 @@
-var dom = document.getElementById("box3_left");
-var myChart = echarts.init(dom);
-var app = {};
-option = null;
-app.title = '极坐标系下的堆叠柱状图';
+var dom = document.getElementById('box3_left')
+var myChart = echarts.init(dom)
+var app = {}
+option = null
+app.title = '极坐标系下的堆叠柱状图'
 
 option = {
-	textStyle:{//图例文字的样式
-                color:'#dbdbdb',
-                fontSize:10
-           },
-    angleAxis: {
+  textStyle: {
+    //图例文字的样式
+    color: '#dbdbdb',
+    fontSize: 10,
+  },
+  angleAxis: {},
+  radiusAxis: {
+    type: 'category',
+    data: ['周一', '周二', '周三', '周四'],
+    z: 10,
+  },
+  polar: {},
+  series: [
+    {
+      type: 'bar',
+      data: [1, 2, 3, 4],
+      coordinateSystem: 'polar',
+      name: 'A',
+      stack: 'a',
     },
-    radiusAxis: {
-        type: 'category',
-        data: ['周一', '周二', '周三', '周四'],
-        z: 10
+    {
+      type: 'bar',
+      data: [2, 4, 6, 8],
+      coordinateSystem: 'polar',
+      name: 'B',
+      stack: 'a',
     },
-    polar: {
+    {
+      type: 'bar',
+      data: [1, 2, 3, 4],
+      coordinateSystem: 'polar',
+      name: 'C',
+      stack: 'a',
     },
-    series: [{
-        type: 'bar',
-        data: [1, 2, 3, 4],
-        coordinateSystem: 'polar',
-        name: 'A',
-        stack: 'a'
-    }, {
-        type: 'bar',
-        data: [2, 4, 6, 8],
-        coordinateSystem: 'polar',
-        name: 'B',
-        stack: 'a'
-    }, {
-        type: 'bar',
-        data: [1, 2, 3, 4],
-        coordinateSystem: 'polar',
-        name: 'C',
-        stack: 'a'
-    }],
-    legend: {
-        show: true,
-        textStyle:{//图例文字的样式
-                color:'#dbdbdb',
-                fontSize:10
-           },
-        data: ['A', 'B', 'C']
-    }
-};
-;
-if (option && typeof option === "object") {
-    myChart.setOption(option, true);
+  ],
+  legend: {
+    show: true,
+    textStyle: {
+      //图例文字的样式
+      color: '#dbdbdb',
+      fontSize: 10,
+    },
+    data: ['A', 'B', 'C'],
+  },
+}
+if (option && typeof option === 'object') {
+  myChart.setOption(option, true)
 }
